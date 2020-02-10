ec697e2946335bc4c2f68d52195e052a5edc4400
samofalov <s.artiom@gmail.com>
2020-02-10 20:04:30 +0200

    New file added with main method.

---
+++ MySQLExample.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class MySQLExample {
+
+	public static void main(String[] args) {
+
+		try {
+			Properties props = new Properties();
+			FileInputStream in = new FileInputStream("db.properties");
+			
+			props.load(in);
+			
+			Connection c = DriverManager.getConnection(
+					props.getProperty("url"), 
+					props.getProperty("username"), 
+					props.getProperty("password"));
+			
+			Statement s = c.createStatement();
+			ResultSet rs = s.executeQuery("select * from customers");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("CustomerNumber"));
+				System.out.println(rs.getString("ContactFirstName"));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
